ca9d203d408e0d850a4e58dd6879fc8656926ed3
johntreynolds <johnt.reynolds@outlook.com>
2026-07-19 17:41:30 -0600

    Completed homing sequence V1. Edited a little bit of the PD controller to bnow consider time.

--- MiniServo4DOFArm.ino
+++ MiniServo4DOFArm.ino
@@ -5,22 +5,32 @@
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40);
 bool robotStopped = false;
 ServoController armController;
-ServoTesting testing;
+ServoTesting testing; 
 
 //=========================START OF PRODUCTION CODE FOR THE ROBOT======================================================
 
 void setup() 
   {
     Serial.begin(9600);
-    testing.beginTest(50);
-    testing.prepServosTest(10);
 
+    //PRODUCTION CODE
+    armController.begin(50);
+    armController.homeArm();
+    //armController.homeTurret();
+   
+    //TESTING
+    //testing.beginTest(50);
+    //testing.prepServosTest(90);
   }
 
-void loop() 
+void loop()
   {
+    //PRODUCTION CODE
+    armController.moveAllServosPD();
+
+    //TESTING
+    //testing.runServoTravelTuning();
     //testing.scanI2C();
-    testing.runServoTravelTuning();
   }
 
 
--- ServoConstants.h
+++ ServoConstants.h
@@ -1,24 +1,19 @@
 #pragma once
 
+//SERVO LOCATIONS
+// Turret Shoulder Elbow Wrist Claw
+// 0      1        2     3     4
 const int MIN_TICK[5]    = {90, 90, 87, 85, 85};
 const int MAX_TICK[5]    = {540, 545, 545, 533, 540};
-const int MIN_ANGLE[5]   = {0, 0, 0, 0, 0};
-const int MAX_ANGLE[5]   = {180, 180, 180, 180, 180};
+
 const int servoArray[5]  = {0, 1, 2, 3, 4};
+const int MIN_ANGLE[5]   = {0, 0, 0, 0, 80};
+const int MAX_ANGLE[5]   = {180, 180, 180, 180, 180};
+
+const int HOMING[5]      = {90, 180, 180, 160, 180};
 
 /*
 
-#pragma once
-
-const int MIN_TICK[5]    = {90, 90, 87, 85, 85};
-const int MAX_TICK[5]    = {540, 545, 545, 533, 540};
-const int MIN_ANGLE[5]   = {0, 0, 0, 0, 0};
-const int MAX_ANGLE[5]   = {180, 180, 180, 180, 180};
-const int servoArray[5]  = {0, 1, 2, 3, 4};
-
-const char* ACTUATOR_NAMES[5] = {
-    "Turret", "Shoulder", "Elbow", "Wrist", "Claw"
-};
 
 
 */
